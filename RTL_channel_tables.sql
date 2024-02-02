05865795dc4c862244fa259ea1fdb9a8464f453c
adhityaraar <adhityaraar@users.noreply.github.com>
2024-02-02 14:51:01 +0700

    upload data

---
+++ RTL_channel_tables.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM demosqlqna.RTL
+LIMIT 0, 1000
+
+-- Date: 2024-02-02 14:48
+*/
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000002,'2023-02-21','2023-02-22','BlueHorizon Oil Services','Pipe A',72,51,48000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000012,'2023-03-14','2023-03-21','BlueHorizon Oil Services','Pipe B',29,66,69000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000013,'2023-03-14','2023-03-21','BlueHorizon Oil Services','Pipe C',12,22,76000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000014,'2023-05-11','2023-05-11','TransCrest Energy Group','Pipe C',100,22,76000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000015,'2023-04-02','2023-04-07','InfraPipe Solutions','Pipe D',5,78,34000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000016,'2023-05-05','2023-05-10','GreenWave Petrochemicals','Pipe D',14,78,34000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000017,'2023-05-05','2023-05-10','GreenWave Petrochemicals','Pipe A',20,51,48000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000018,'2023-02-08','2023-02-14','GreenWave Petrochemicals','Pipe D',22,78,34000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000019,'2023-02-21','2023-02-22','BlueHorizon Oil Services','Pipe B',50,66,69000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000020,'2023-02-08','2023-02-14','InfraPipe Solutions','Pipe C',34,22,76000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000021,'2023-02-08','2023-02-14','TransCrest Energy Group','Pipe C',43,22,76000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000022,'2023-06-08','2023-06-30','TransCrest Energy Group','Pipe A',98,51,48000);
---
+++ SJR_channel_tables.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM demosqlqna.SJR
+LIMIT 0, 1000
+
+-- Date: 2024-02-02 14:49
+*/
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2021000025,'2023-06-02','2023-06-07','PetroLink Solutions','Fitting J',8,28,72000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000003,'2023-05-18','2023-05-27','AquaVista Petroleum','Fitting F',55,16,55000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000004,'2023-02-22','2023-02-25','PipeConnect Ventures','Fitting F',22,34,55000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000005,'2023-02-22','2023-02-25','PipeConnect Ventures','Fitting E',56,30,37000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000006,'2023-06-22','2023-06-25','AquaVista Petroleum','Fitting E',103,143,37000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000007,'2023-03-05','2023-03-22','AquaVista Petroleum','Fitting G',72,76,92000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000008,'2023-03-14','2023-03-21','PetroLink Solutions','Fitting G',46,87,92000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000009,'2023-03-14','2023-03-21','PipeConnect Ventures','Fitting G',16,92,92000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000010,'2023-04-21','2023-04-24','EnergyFlow Industries','Fitting H',66,42,38000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000011,'2023-05-05','2023-05-14','EnergyFlow Industries','Fitting I',75,24,45000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000023,'2023-05-05','2023-05-14','AquaVista Petroleum','Fitting I',22,75,45000);
+INSERT INTO `` (`numberID`,`orderDate`,`invoiceDate`,`customer`,`itemProduct`,`itemQty`,`itemKG`,`itemPrice`) VALUES (2023000025,'2023-06-08','2023-06-19','EnergyFlow Industries','Fitting J',44,16,72000);
